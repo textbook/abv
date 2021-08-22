57581f0fef807d13ac6b901858d7b1a793745a30
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2021-08-22 22:51:57 +0100

    Refactor event handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,45 @@
-import { useEffect, useState } from "react";
+import { ChangeEventHandler, useState } from "react";
 
 const FACTOR = 2.5 / 29.57;
 
 function App() {
-  const [abv, setAbv] = useState(0);
-  const [volume, setVolume] = useState(0);
-  const [calories, setCalories] = useState(0);
+  const [data, setData] = useState({ abv: 0, volume: 0 });
+  const calories = Math.round(data.abv * data.volume * FACTOR);
 
-  useEffect(() => {
-    setCalories(Math.round(abv * volume * FACTOR));
-  }, [abv, volume]);
+  const changeHandler: ChangeEventHandler<HTMLInputElement> = ({ target: { name, value } }) => {
+    setData({ ...data, [name]: parseFloat(value) });
+  }
 
   return (
     <div>
       <label>
         ABV (%)
         <input
-          onChange={({ target: { value } }) => setAbv(parseFloat(value))}
+          name="abv"
+          onChange={changeHandler}
           type="number"
-          value={abv}
+          value={data.abv}
         />
       </label>
 
       <label>
         Volume (ml)
         <input
-          onChange={({ target: { value } }) => setVolume(parseFloat(value))}
+          name="volume"
+          onChange={changeHandler}
           type="number"
-          value={volume}
+          value={data.volume}
         />
       </label>
 
       <label>
         Calories
-        <input readOnly name="calories" type="text" value={`${calories}`} />
+        <input
+          name="calories"
+          readOnly
+          type="text"
+          value={`${calories}`}
+        />
       </label>
     </div>
   );
