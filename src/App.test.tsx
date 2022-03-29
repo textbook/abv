2c5b7f3846615faf92909437c51c30e880b4429d
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2022-03-29 22:03:29 +0100

    Extract selection helpers

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,49 +6,58 @@ import App from "./App";
 describe("App component", () => {
 	it("shows the calories for 330ml", () => {
 		render(<App />);
-		userEvent.type(screen.getByRole("spinbutton", { name: "ABV (%)" }), "4.5");
-		userEvent.type(screen.getByRole("spinbutton", { name: "Volume (ml)" }), "330");
-		expect(screen.getByRole("textbox", { name: "Calories" })).toHaveValue("126");
+		userEvent.type(getAbvInput(), "4.5");
+		userEvent.type(getVolumeInput(), "330");
+		expect(getCaloriesTextbox()).toHaveValue("126");
 	});
 
 	it("provides a shortcut for 330ml", () => {
 		render(<App />);
-		userEvent.type(screen.getByRole("spinbutton", { name: "ABV (%)" }), "4.5");
-		userEvent.click(screen.getByRole("button", { name: "330ml" }));
-		expect(screen.getByRole("textbox", { name: "Calories" })).toHaveValue("126");
+		userEvent.type(getAbvInput(), "4.5");
+		const volume = "330ml";
+		userEvent.click(getVolumeButton(volume));
+		expect(getCaloriesTextbox()).toHaveValue("126");
 	});
 
 	it("shows the calories for 440ml", () => {
 		render(<App />);
-		userEvent.type(screen.getByRole("spinbutton", { name: "ABV (%)" }), "4.8");
-		userEvent.type(screen.getByRole("spinbutton", { name: "Volume (ml)" }), "440");
-		expect(screen.getByRole("textbox", { name: "Calories" })).toHaveValue("179");
+		userEvent.type(getAbvInput(), "4.8");
+		userEvent.type(getVolumeInput(), "440");
+		expect(getCaloriesTextbox()).toHaveValue("179");
 	});
 
 	it("provides a shortcut for 440ml", () => {
 		render(<App />);
-		userEvent.type(screen.getByRole("spinbutton", { name: "ABV (%)" }), "4.8");
-		userEvent.click(screen.getByRole("button", { name: "440ml" }));
-		expect(screen.getByRole("textbox", { name: "Calories" })).toHaveValue("179");
+		userEvent.type(getAbvInput(), "4.8");
+		userEvent.click(getVolumeButton("440ml"));
+		expect(getCaloriesTextbox()).toHaveValue("179");
 	});
 
 	it("shows the calories for a pint", () => {
 		render(<App />);
-		userEvent.type(screen.getByRole("spinbutton", { name: "ABV (%)" }), "5");
-		userEvent.type(screen.getByRole("spinbutton", { name: "Volume (ml)" }), "568");
-		expect(screen.getByRole("textbox", { name: "Calories" })).toHaveValue("240");
+		userEvent.type(getAbvInput(), "5");
+		userEvent.type(getVolumeInput(), "568");
+		expect(getCaloriesTextbox()).toHaveValue("240");
 	});
 
 	it("provides a shortcut for a pint", () => {
 		render(<App />);
-		userEvent.type(screen.getByRole("spinbutton", { name: "ABV (%)" }), "5");
-		userEvent.click(screen.getByRole("button", { name: "568ml" }));
-		expect(screen.getByRole("textbox", { name: "Calories" })).toHaveValue("240");
+		userEvent.type(getAbvInput(), "5");
+		userEvent.click(getVolumeButton("568ml"));
+		expect(getCaloriesTextbox()).toHaveValue("240");
 	});
 
 	it("gracefully handles NaN", () => {
 		render(<App />);
-		userEvent.clear(screen.getByRole("spinbutton", { name: "ABV (%)" }));
-		expect(screen.getByRole("textbox", { name: "Calories" })).toHaveValue("0");
+		userEvent.clear(getAbvInput());
+		expect(getCaloriesTextbox()).toHaveValue("0");
 	});
 });
+
+const getAbvInput = () => screen.getByRole("spinbutton", { name: "ABV (%)" });
+
+const getCaloriesTextbox = () => screen.getByRole("textbox", { name: "Calories" });
+
+const getVolumeButton = (volume: string) => screen.getByRole("button", { name: volume });
+
+const getVolumeInput = () => screen.getByRole("spinbutton", { name: "Volume (ml)" });
