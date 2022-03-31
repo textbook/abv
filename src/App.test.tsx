dc27718f256bffd97502b5e676aadbc4dfe29a55
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2022-03-31 14:47:28 +0100

    Upgrade to latest Testing Library user-events

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,52 +4,52 @@ import userEvent from "@testing-library/user-event";
 import App from "./App";
 
 describe("App component", () => {
-	it("shows the calories for 330ml", () => {
+	it("shows the calories for 330ml", async () => {
 		render(<App />);
-		userEvent.type(getAbvInput(), "4.5");
-		userEvent.type(getVolumeInput(), "330");
+		await userEvent.type(getAbvInput(), "4.5");
+		await userEvent.type(getVolumeInput(), "330");
 		expect(getCaloriesTextbox()).toHaveValue("126");
 	});
 
-	it("provides a shortcut for 330ml", () => {
+	it("provides a shortcut for 330ml", async () => {
 		render(<App />);
-		userEvent.type(getAbvInput(), "4.5");
+		await userEvent.type(getAbvInput(), "4.5");
 		const volume = "330ml";
-		userEvent.click(getVolumeButton(volume));
+		await userEvent.click(getVolumeButton(volume));
 		expect(getCaloriesTextbox()).toHaveValue("126");
 	});
 
-	it("shows the calories for 440ml", () => {
+	it("shows the calories for 440ml", async () => {
 		render(<App />);
-		userEvent.type(getAbvInput(), "4.8");
-		userEvent.type(getVolumeInput(), "440");
+		await userEvent.type(getAbvInput(), "4.8");
+		await userEvent.type(getVolumeInput(), "440");
 		expect(getCaloriesTextbox()).toHaveValue("179");
 	});
 
-	it("provides a shortcut for 440ml", () => {
+	it("provides a shortcut for 440ml", async () => {
 		render(<App />);
-		userEvent.type(getAbvInput(), "4.8");
-		userEvent.click(getVolumeButton("440ml"));
+		await userEvent.type(getAbvInput(), "4.8");
+		await userEvent.click(getVolumeButton("440ml"));
 		expect(getCaloriesTextbox()).toHaveValue("179");
 	});
 
-	it("shows the calories for a pint", () => {
+	it("shows the calories for a pint", async () => {
 		render(<App />);
-		userEvent.type(getAbvInput(), "5");
-		userEvent.type(getVolumeInput(), "568");
+		await userEvent.type(getAbvInput(), "5");
+		await userEvent.type(getVolumeInput(), "568");
 		expect(getCaloriesTextbox()).toHaveValue("240");
 	});
 
-	it("provides a shortcut for a pint", () => {
+	it("provides a shortcut for a pint", async () => {
 		render(<App />);
-		userEvent.type(getAbvInput(), "5");
-		userEvent.click(getVolumeButton("568ml"));
+		await userEvent.type(getAbvInput(), "5");
+		await userEvent.click(getVolumeButton("568ml"));
 		expect(getCaloriesTextbox()).toHaveValue("240");
 	});
 
-	it("gracefully handles NaN", () => {
+	it("gracefully handles NaN", async () => {
 		render(<App />);
-		userEvent.clear(getAbvInput());
+		await userEvent.clear(getAbvInput());
 		expect(getCaloriesTextbox()).toHaveValue("0");
 	});
 });
